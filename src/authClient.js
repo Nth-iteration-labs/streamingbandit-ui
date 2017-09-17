26e676bc68c0a68446e4ac9f1f4beb7b08100561
Robin van Emden <robin@pwy.nl>
2017-09-17 15:02:23 +0200

    First commit source code
    First version of StreamingBandit UI, build against
    https://github.com/Nth-iteration-labs/streamingbandit/tree/4697e996e44683e88b0ada50dd5554f0a32e5131
    
    Login works, basic calls work. Next version will make use of the added
    status codes in StreamingBandit version:
    https://github.com/Nth-iteration-labs/streamingbandit/tree/1016b4a7c5e99e0b78ed271f760e01147e94d198

---
+++ src/authClient.js
@@ -0,0 +1,48 @@
+// in src/authClient.js
+import { AUTH_LOGIN, AUTH_LOGOUT, AUTH_ERROR, AUTH_CHECK } from 'admin-on-rest';
+
+export default (type, params) => {
+
+    if (type === AUTH_LOGIN) {
+
+        const { username, password } = params;
+
+		const request = new Request('http://strm.mnds.org:7070/login.secure', {method: 'POST',  
+			body: 'username='+username+'&password='+password,
+			headers: new Headers({ 'Content-Type': 'application/x-www-form-urlencoded' })
+		});
+
+        return fetch(request)
+			.then((response) => response.text())     // Transform the response into text
+		    .then((data) => {
+                if (data.includes('rong')) {
+					return Promise.reject()
+                } else {
+					localStorage.setItem('username', username)
+					return Promise.resolve()
+				}
+            })
+		    .catch((e) => {
+				//console.log("caught")
+		    })
+    }
+
+    if (type === AUTH_LOGOUT) {
+        localStorage.removeItem('username');
+        return Promise.resolve();
+    }
+
+    if (type === AUTH_ERROR) {
+        const { status } = params;
+        if (status === 401 || status === 403) {
+            localStorage.removeItem('username');
+            return Promise.reject();
+        }
+        return Promise.resolve();
+    }
+
+    if (type === AUTH_CHECK) {
+        return localStorage.getItem('username') ? Promise.resolve() : Promise.reject();
+    }
+    return Promise.reject('Unknown method');
+};

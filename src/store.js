dfd98fed1c6cc722853dd4f4d9fc6768cbd162fe
Robin van Emden <robin@pwy.nl>
2017-10-01 13:23:50 +0200

    Fix for login

--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ import { easyStore } from 'react-easy-state'
 import { easyParams } from 'react-easy-params'
 
 const store = easyStore({
-  serverurl: "http://localhost:8080",
+  serverurl: "http://strm.mnds.org:7070",
   setServerUrl (ev) {
     this.serverurl = ev
   }

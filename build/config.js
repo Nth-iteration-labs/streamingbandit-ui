134a7747527319de377e6d5072787f26c2db402b
Robin van Emden <robin@pwy.nl>
2017-10-12 15:38:51 +0200

    Latest build

---
+++ build/config.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ * StreamingBandit configuration file
+ *
+ * Makes use of JS global variable 
+ * Namespace set to sb_config
+ *
+ */
+
+// Do not touch this line
+
+window.sb_config = {};
+
+// Set default server URL 
+
+sb_config.set_server_url = "http://localhost:8080";
